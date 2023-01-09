f3b76890aadfba94b31a89e53b1a474f82612a7f
Sabri10600 <sabrisaoudi@gmail.com>
2023-01-09 15:46:32 +0100

    BOSS_DEPLACEMENT

--- BeatEmUp/Assets/BOSS_MOVEMENT.cs
+++ BeatEmUp/Assets/BOSS_MOVEMENT.cs
@@ -4,49 +4,107 @@ using UnityEngine;
 
 public class BOSS_MOVEMENT : MonoBehaviour
 {
-    [SerializeField] float speed = 5f;
-    Rigidbody2D bossrb;
-    public bool _moveRight = true;
-    float bossSpeed = 500;
-    private float _startPos;
-    private float _endPos;
-    public bool _isFacingRight;
-    public int UnitsToMove = 5;
-  
-    
+    [SerializeField] GameObject Boss;
+    [SerializeField] float bossSpeed = 5f;
+    Vector2 bossDir;
+    [SerializeField] Animator animator;
+    [SerializeField] BossState currentState;
+
+    Rigidbody2D rb2d;
+
+    public enum BossState
+    {
+        IDLE,
+        WALK
+    }
+     
+
+
 
 
     // Start is called before the first frame update
-    public void Awake()
+     void Start()
     {
-        bossrb = GetComponent<Rigidbody2D>();
-        _startPos = transform.position.x;
-        _endPos = _startPos + UnitsToMove;
-        _isFacingRight = transform.localScale.x > 0;
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
-   
+
 
     // Update is called once per frame
     void Update()
     {
-        if (_moveRight)
+
+        if (Boss != null)
         {
-            bossrb.AddForce(Vector2.right * bossSpeed * Time.deltaTime);
-            if (!_isFacingRight)
-                Flip();
+            transform.LookAt(Boss.transform.position);
         }
 
-        if (bossrb.position.x >= _endPos)
-            _moveRight = false;
-
-       
+        
 
 
     }
-    public void Flip()
+
+    void OnStateExit()
     {
-        transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
-        _isFacingRight = transform.localScale.x > 0;
+        switch (currentState)
+        {
+            case BossState.IDLE:
+                break;
+            case BossState.WALK:
+                break;
+            default:
+                break;
+        }
+    }
+
+    void OnStateEnter()
+    {
+        switch (currentState)
+        {
+            case BossState.IDLE:
+                break;
+
+            case BossState.WALK:
+
+            default:
+                break;
+        }
+    }
+
+
+
+    void OnStateUpdate()
+    {
+        switch (currentState)
+        {
+            case BossState.IDLE:
+                if (bossDir != Vector2.zero)
+                {
+                    TransitionToState(BossState.WALK);
+                }
+                break;
+            case BossState.WALK:
+                break;
+            default:
+                break;
+        }
+    }
+
+    void GetInputs()
+    {
+        bossDir = new Vector2(Input.GetAxisRaw("Horizontal_Boss"), Input.GetAxisRaw("Vertical_Boss"));
+
+        if(bossDir.magnitude != 0)
+        {
+            animator.SetBool("WALK", true);
+        }
+    }
+
+    void TransitionToState(BossState nextState)
+    {
+        OnStateExit();
+        currentState = nextState;
+        OnStateEnter();
+
     }
 }
