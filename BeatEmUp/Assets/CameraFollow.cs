0b89110adf81358d2d376ab54c3d1004bf12e71a
Rozla <vincent.laroze.vl@gmail.com>
2023-01-14 13:38:32 +0100

    CameraMovement ok !

--- BeatEmUp/Assets/CameraFollow.cs
+++ BeatEmUp/Assets/CameraFollow.cs
@@ -5,11 +5,21 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     [SerializeField] Transform playerTransform;
+    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] Vector3 offset = Vector3.zero;
+    [SerializeField] BoxCollider2D cameraBounds;
+    [SerializeField] bool canFollow;
+
+    Camera mainCamera;
+    Vector2 cameraDimension;
 
 
-    Vector3 dirToFollow;
-
-
+    private void Awake()
+    {
+        mainCamera = GetComponent<Camera>();
+        cameraDimension.y = mainCamera.orthographicSize;
+        cameraDimension.x = mainCamera.orthographicSize * mainCamera.aspect;
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -19,10 +29,24 @@ public class CameraFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(playerTransform != null)
+        if (!canFollow)
         {
-            dirToFollow = new Vector3(playerTransform.position.x, transform.position.y, transform.position.z);
-            transform.position = dirToFollow;
+            return;
         }
+
+        Vector3 followingPosition = playerTransform.position + offset;
+
+        float minX = cameraBounds.transform.position.x - cameraBounds.size.x / 2 + cameraDimension.x;
+        float maxX = cameraBounds.transform.position.x + cameraBounds.size.x / 2 - cameraDimension.x;
+        followingPosition.x = Mathf.Clamp(followingPosition.x, minX, maxX);
+
+        float minY = cameraBounds.transform.position.y - cameraBounds.size.y / 2 + cameraDimension.y;
+        float maxY = cameraBounds.transform.position.y + cameraBounds.size.y / 2 - cameraDimension.y;
+        followingPosition.y = Mathf.Clamp(followingPosition.y, minY, maxY);
+
+        Vector3 currentVelocity = Vector3.zero;
+        transform.position = Vector3.SmoothDamp(transform.position, followingPosition, ref currentVelocity, Time.deltaTime * moveSpeed);
+
     }
+
 }
