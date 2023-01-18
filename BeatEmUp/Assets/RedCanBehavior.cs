efb155ec6914feb1a7427346f9591fb2a9f850d6
Rozla <vincent.laroze.vl@gmail.com>
2023-01-18 15:11:35 +0100

    Distributeur complètement terminé !!

--- BeatEmUp/Assets/RedCanBehavior.cs
+++ BeatEmUp/Assets/RedCanBehavior.cs
@@ -7,8 +7,29 @@ public class RedCanBehavior : MonoBehaviour
 
     float t = 5f;
 
+    float moveTimer = 0f;
+
+    float dirX;
+    float dirY;
+
+    private void Start()
+    {
+        dirX = Random.Range(9f, 11f);
+        dirY = Random.Range(-5f, 5f);
+    }
+
     private void Update()
     {
+
+        moveTimer += Time.deltaTime;
+
+        if (moveTimer > .2f && moveTimer < .5f)
+        {
+            transform.Translate(Vector3.down * dirX * Time.deltaTime);
+            transform.Translate(Vector3.right * dirY * Time.deltaTime);
+        }
+
+
         t -= Time.deltaTime;
 
         if(t <= 0f)
