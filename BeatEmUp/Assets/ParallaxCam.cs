fd30cd18e4ac4df304fc39bd4cf7fdeed45f0952
Rozla <vincent.laroze.vl@gmail.com>
2023-01-23 16:50:14 +0100

    On part en build

--- BeatEmUp/Assets/ParallaxCam.cs
+++ BeatEmUp/Assets/ParallaxCam.cs
@@ -10,14 +10,18 @@ public class ParallaxCam : MonoBehaviour
 
     [SerializeField] float[] scrollSpeed;
 
+
+    float t;
     private void FixedUpdate()
     {
+        t += Time.fixedDeltaTime;
+
         for (int background = 0; background < backgrounds.Length; background++)
         {
 
             Renderer rend = backgrounds[background].GetComponent<Renderer>();
 
-            float offset = Time.time * (scrollSpeed[background] + additionalScrollSpeed);
+            float offset = t * (scrollSpeed[background] + additionalScrollSpeed);
 
             rend.material.SetTextureOffset("_MainTex", new Vector2( 0, offset));
 
